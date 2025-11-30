c1347f0ff1ecf7bccd5649f5df9715acef3389cb
Saurya <coolsaurya2010@gmail.com>
2025-11-29 21:32:32 -0500

    Reformatted Constants file

--- Oscar/Constants.h
+++ Oscar/Constants.h
@@ -15,99 +15,97 @@
 #define DATA_PIN 39                 //DATA PIN 5 (GPIO D5)                         
 CRGB leds[NUM_LEDS];
 
-double distance = 0;
-double heading = 0; 
-double roll = 0;
-double pitch = 0;
-double pivotPosition = 0;
+//General robot variables
+  double distance = 0;
+  double heading = 0; 
+  double roll = 0;
+  double pitch = 0;
+  int currentTime = 0;
+  int previousTime = 0;
+  enum {
+    START,
+    CORAL,
+    ALGAE
+  } STATE;
 
-int currentTime = 0;
-int previousTime = 0;
-int servoGoal = 0;
-int pivotGoal = 0;
-int pivotError = 0;
-int lastModeSwitch = 0;
-
-const int distanceTrig = 11;
-const int distanceEcho = 10;
 
 //diff variables to be used in controls to control delay
-double diffSTOW = 0;
-double diffL2 = 0;
-double diffL3 = 0;
-double diffL4 = 0;
-double diffAL2 = 0;
-double diffAL3 = 0;
-double diffBarge = 0;
+  int diffSTOW = 0;
+  int diffL2 = 0;
+  int diffL3 = 0;
+  int diffL4 = 0;
+  int diffAL2 = 0;
+  int diffAL3 = 0;
+  int diffBarge = 0;
+  int lastModeSwitch = 0;
 
-//pivotPID constants
-const double pivotKp = 0.2;
-const double pivotKd = 0.1;
-const double pivotKi = 0.005; 
-const double pivotMin = -1;
-const double pivotMax = 1; 
+//Pivot variables
+  int pivotGoal = 0;
+  int pivotError = 0;
+  double pivotPosition = 0;
+  //pivotPID constants
+    const float pivotKp = 0.2;
+    const float pivotKd = 0.1;
+    const float pivotKi = 0.005; 
+    const float pivotMin = -1;
+    const float pivotMax = 1; 
 
-//Preset values for elevator
-const double servoSTOW = 0;
-const double servoReady = 20;
-const double servoL2 = 0;
-const double servoL3 = 0;
-const double servoL4 = 140;
-const double servoAL2 = 120;
-const double servoAL3 = 0;
-const double servoBarge = 180;
+  //Preset values for pivot
+    const int pivotSTOW = 0;
+    const int pivotReady = 40;
+    const int pivotL2 = 360;
+    const int pivotL3 = 500;
+    const int pivotL4 = 500;
+    const int pivotAL2 = 0;
+    const int pivotAL3 = 600;
+    const int pivotBarge = 700;
 
-//Preset values for pivot
-const double pivotSTOW = 0;
-const double pivotReady = 40;
-const double pivotL2 = 360;
-const double pivotL3 = 500;
-const double pivotL4 = 500;
-const double pivotAL2 = 0;
-const double pivotAL3 = 600;
-const double pivotBarge = 700;
+//Variables for elevator
+  int servoGoal = 0;
+  //Preset values for elevator
+    const int servoSTOW = 0;
+    const int servoReady = 20;
+    const int servoL2 = 0;
+    const int servoL3 = 0;
+    const int servoL4 = 140;
+    const int servoAL2 = 120;
+    const int servoAL3 = 0;
+    const int servoBarge = 180;
 
 //Bindings for Xbox controller
-const int buttonA = 0;
-const int buttonB = 1;
-const int buttonX = 2;
-const int buttonY = 3;
-const int leftBumper = 4;
-const int rightBumper = 5; 
-const int leftTrigger = 6;
-const int rightTrigger = 7;
-const int leftMain = 8;
-const int rightMain = 9;
-const int leftStick = 10;
-const int rightStick = 11;
-const int upDPad = 12;
-const int downDPad = 13;
-const int leftDPad = 14;
-const int rightDPad = 15;
+  const int buttonA = 0;
+  const int buttonB = 1;
+  const int buttonX = 2;
+  const int buttonY = 3;
+  const int leftBumper = 4;
+  const int rightBumper = 5; 
+  const int leftTrigger = 6;
+  const int rightTrigger = 7;
+  const int leftMain = 8;
+  const int rightMain = 9;
+  const int leftStick = 10;
+  const int rightStick = 11;
+  const int upDPad = 12;
+  const int downDPad = 13;
+  const int leftDPad = 14;
+  const int rightDPad = 15;
 
-//swerve vars
-double turnMag = 0.0;
-double driveAngle = 0.0;
-double driveMag = 0.0;
-double drivetrainVectors[4][2] = { { 0, 0 },
-                                   { 0, 0 },
-                                   { 0, 0 },
-                                   { 0, 0 } };
+//Swerve vars
+  double turnMag = 0.0;
+  double driveAngle = 0.0;
+  double driveMag = 0.0;
+  double drivetrainVectors[4][2] = { { 0, 0 },
+                                    { 0, 0 },
+                                    { 0, 0 },
+                                    { 0, 0 } };
 
-double theta;
-double headingOffset = 0.0;
-int mod1Offset = 0;
-int mod2Offset = 0;
-int mod3Offset = 0;
-int mod4Offset = 0;
-int lastOffsetTime = millis();
-
-const bool AM_DEBUGGING = false;
-
-enum {
-  START,
-  CORAL,
-  ALGAE
-} STATE;
+  double theta;
+  double headingOffset = 0.0;
+  int mod1Offset = 0;
+  int mod2Offset = 0;
+  int mod3Offset = 0;
+  int mod4Offset = 0;
+  int lastOffsetTime = millis();
+  const bool AM_DEBUGGING = false;
 
 #endif
